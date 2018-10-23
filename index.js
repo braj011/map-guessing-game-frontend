686ffba4413745820faee5f8820c03fbcc93f536
braj011 <bhagyaraj01@gmail.com>
2018-10-23 16:16:42 +0100

    staretd front end, created basic welcome screen, username and difficulty level

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ const updateMap = (lat, lon, zoom) => {
   imgEl.src = getMapUrl(lat, lon, zoom)
 }
 
-updateMap(51.52089, -0.08649, 15)
+// toggle mapContainer & welcomeContainer display
+let mapContainerToggle = document.getElementById('map-container')
+let welcomeContainerToggle = document.getElementById('welcome-container')
+mapContainerToggle.style.display = 'none'
+welcomeContainerToggle.style.display = 'flex'
 
+function mapOffWelcomeOn () {
+  mapContainerToggle.style.display = 'none'
+  welcomeContainerToggle.style.display = 'flex'
+}
+function mapOnWelcomeOff () {
+  mapContainerToggle.style.display = 'flex'
+  welcomeContainerToggle.style.display = 'none'
+}
+
+// submit username button - triggers mapOnWelcomeOff()
+
+const usernameSubmit = document.getElementById('submit-un-btn')
+usernameSubmit.addEventListener('click', () => {
+  mapOnWelcomeOff()
+})
+
+updateMap(51.52089, -0.08649, 15)
 
 // Full URL with style guide
 // &format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xebe3cd&style=element:labels.text.fill%7Ccolor:0x523735&style=element:labels.text.stroke%7Ccolor:0xf5f1e6&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0xc9b2a6&style=feature:administrative.land_parcel%7Celement:geometry.stroke%7Ccolor:0xdcd2be&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xae9e90&style=feature:administrative.locality%7Celement:labels.text%7Cvisibility:off&style=feature:administrative.neighborhood%7Celement:labels.text%7Cvisibility:off&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x93817c&style=feature:poi.business%7Celement:labels%7Cvisibility:off&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0xa5b076&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x447530&style=feature:poi.place_of_worship%7Celement:labels%7Cvisibility:off&style=feature:poi.school%7Celement:labels.text%7Cvisibility:off&style=feature:road%7Celement:geometry%7Ccolor:0xf5f1e6&style=feature:road.arterial%7Celement:geometry%7Ccolor:0xfdfcf8&style=feature:road.highway%7Celement:geometry%7Ccolor:0xf8c967&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0xe9bc62&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0xe98d58&style=feature:road.highway.controlled_access%7Celement:geometry.stroke%7Ccolor:0xdb8555&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x806b63&style=feature:transit.line%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:transit.line%7Celement:labels.text.fill%7Ccolor:0x8f7d77&style=feature:transit.line%7Celement:labels.text.stroke%7Ccolor:0xebe3cd&style=feature:transit.station%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:transit.station.rail%7Celement:labels.text%7Cvisibility:off&style=feature:water%7Celement:geometry.fill%7Ccolor:0xb9d3c2&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x92998d
