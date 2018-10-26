8d01d363d2ea4f57f6465077e797788455430cc6
Ciaran M <cmorinan@gmail.com>
2018-10-26 13:58:51 +0100

    wrapped in basic express server

---
+++ public/src/API.js
@@ -0,0 +1,27 @@
+class API {
+  static init () {
+    this.baseUrl = 'https://map-guessing-game.herokuapp.com/api/v1/'
+    // this.baseUrl = 'http://localhost:3000/api/v1/'
+  }
+
+  static getRandomLocation (difficulty, seed) {
+    return fetch(this.baseUrl + 
+      `areas?difficulty=${difficulty}&seed=${seed}`)
+      .then(resp => resp.json())
+  }
+
+  static getHighScores() {
+    return fetch(this.baseUrl + 'scores')
+      .then(resp => resp.json())
+  }
+
+  static postUserScore (userScore) {
+    return fetch(this.baseUrl + 'scores', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(userScore)
+    }).then(resp => resp.json())
+  }
+}
+
+API.init()
---
+++ public/src/index.js
@@ -0,0 +1,85 @@
+const nameDisplay = document.getElementById('name-display')
+const nameInput = document.getElementById('name-input')
+const mapContainer = document.getElementById('map-container')
+const welcomeContainer = document.getElementById('welcome-container')
+const difficultyContainer = document.getElementById('difficulty-container')
+const highScores = document.getElementById('high-scores')
+const scoreTable = document.getElementById('score-table')
+const guesses = document.getElementById('guesses')
+const answerText = document.getElementById('answer')
+const scoreDisplay = document.getElementById('game-score-display')
+const timeDisplay = document.getElementById('game-timer-display')
+const guessTable = document.getElementById('guess-table')
+const readyText = document.getElementById('ready')
+const mapImage = document.querySelector('#map')
+
+const startInput = function(event) {
+  if (nameInput.value === '') {
+    nameInput.focus()
+  } else if (event.keyCode === 13 && scoreDisplay.innerText === 'SCORE') {
+    nameInput.removeEventListener('keypress', startInput, true)
+    game("easy")
+  } else if (event.target.classList.contains('difficulty')) {
+    selectedDif = event.target.id
+    game(selectedDif)
+  }
+}
+
+difficultyContainer.addEventListener('click', startInput, true)
+nameInput.addEventListener('keypress', startInput, true)
+
+function mapOnWelcomeOff() {
+  displaySquares()
+  scoreTable.innerHTML = ''
+  welcomeContainer.style.display = 'none'
+  mapContainer.style.display = 'flex'
+  highScores.style.display = 'none'
+  guesses.style.display = 'block'
+}
+
+function mapOffWelcomeOn() {
+  nameInput.addEventListener('keypress', startInput, true)
+  mapContainer.removeEventListener('click', mapOffWelcomeOn)
+  document.removeEventListener('keyup', keyRestart)
+  answerText.style.display = 'none'
+  scoreDisplay.innerText = 'SCORE'
+  timeDisplay.innerText = '30s'
+  guessTable.innerHTML = ''
+  scoreTable.innerHTML = ''
+  showHighScores()
+  mapContainer.style.cursor = null
+  mapContainer.style.display = 'none'
+  guesses.style.display = 'none'
+  welcomeContainer.style.display = 'flex'
+  highScores.style.display = 'block'
+  nameInput.focus()
+}
+
+function keyRestart(event) {
+  event.preventDefault()
+  mapOffWelcomeOn()
+}
+
+function randomNum(limit) {
+  return Math.floor(Math.random() * limit)
+}
+
+function showHighScores() {
+  API.getHighScores()
+    .then(data => data.forEach(renderScore))
+}
+
+function renderScore(score, userScore=false) {
+  scoreEl = document.createElement('tr')
+  scoreEl.innerHTML = `
+      <td>${score.rank}</td>
+      <td>${score.username}</td>
+      <td>${score.score}</td>
+    `
+  if (score.rank === userScore.rank) {
+        scoreEl.classList.add('blink')
+      }
+  scoreTable.appendChild(scoreEl)
+}
+
+showHighScores()
